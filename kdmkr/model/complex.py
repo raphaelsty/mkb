118de3bf1402b8bf4fd75e1af3fb9bc978b0d5cd
raphaelsty <raphael.sourty@gmail.com>
2020-06-23 15:54:54 +0200

    Add topk to complex

--- kdmkr/model/complex.py
+++ kdmkr/model/complex.py
@@ -9,8 +9,8 @@ class ComplEx(base.Teacher):
     """ComplEx"""
 
     def __init__(self, hidden_dim, n_entity, n_relation, gamma):
-        super().__init__(hidden_dim=hidden_dim, relation_dim=hidden_dim, entity_dim=hidden_dim*2,
-            n_entity=n_entity, n_relation=n_relation*2, gamma=gamma)
+        super().__init__(hidden_dim=hidden_dim, relation_dim=hidden_dim*2, entity_dim=hidden_dim*2,
+            n_entity=n_entity, n_relation=n_relation, gamma=gamma)
 
     def forward(self, sample, mode='default'):
         head, relation, tail = self.head_relation_tail(sample=sample, mode=mode)
@@ -28,7 +28,6 @@ class ComplEx(base.Teacher):
             score = re_score * re_tail + im_score * im_tail
         return score.sum(dim = 2)
 
-
     def distill(self, sample):
         head, relation, tail = self.distillation_batch(sample)
         re_head, im_head = torch.chunk(head, 2, dim=-1)
@@ -39,3 +38,11 @@ class ComplEx(base.Teacher):
         im_score = re_relation * im_tail - im_relation * re_tail
         score = re_head * re_score + im_head * im_score
         return score.sum(dim = -1)
+
+    def _top_k(self, sample):
+        """Method dedicated to compute the top k entities and relations for a given triplet."""
+        head, relation, tail = self.head_relation_tail(sample=sample, mode='default')
+        embedding_head     = - relation + tail
+        embedding_relation = - head + tail
+        embedding_tail     = head + relation
+        return embedding_head, embedding_relation, embedding_tail
