438cb1a997d663ac4bc895771d24158307ce3e52
raphaelsty <raphael.sourty@gmail.com>
2020-07-07 18:07:25 +0200

    Update KgeBoard

--- kdmkr/evaluation/kge_board.py
+++ kdmkr/evaluation/kge_board.py
@@ -23,25 +23,41 @@ class KGEBoard:
         if self.key_metric:
             self.best_params = collections.defaultdict(lambda: collections.defaultdict(float))
 
-    def update(self, model, step, metrics, prefix=''):
-        description = f'{model} {self._description(x=metrics)}'
+
+    def update(self, model, step, metrics, **description):
+        description = f'{model} {self._description(**description)}'
 
         for m, s in metrics.items():
             self.writer.add_scalars(
-                main_tag=f'{prefix}_{m}', tag_scalar_dict={f'{model}': s}, global_step=step)
+                main_tag=f'{self.experiment}_{m}', tag_scalar_dict={f'{model}': s}, global_step=step)
 
-        # Write on tensorboard best models
-        if self.key_metric and self.best_params[model][self.key_metric] < metrics[self.key_metric]:
+        if self.key_metric in metrics and self.best_params[model][self.key_metric] < metrics[self.key_metric]:
             self.best_params.pop(model)
             for m, s in metrics.items():
                 self.best_params[f'{self.experiment}_{model}'][m] = s
             self.best_params[f'{self.experiment}_{model}']['step'] = step
 
+
+    def export_best_scores(self, model=None):
+        '''
+        Export best scores founds for each model by default.
+        If model is specified, it export the best scores found for this model.
+        '''
+        if model is not None:
+            self.writer.add_text(
+                tag         = f'{model}',
+                text_string = self._description(**self.best_params[f'{model}']),
+                global_step = self.best_params[f'{model}']['step']
+            )
+
+        else:
+            for model, scores in self.best_params.items():
+                self.writer.add_text(
+                    tag         = model,
+                    text_string = self._description(**scores),
+                    global_step = scores['step']
+                )
+
     @classmethod
-    def _description(cls, x):
+    def _description(cls, **x):
         return ', '.join([f'{xi}: {yi}' for xi, yi in x.items()])
-
-
-    def export_best_params(self):
-        for model, scores in self.best_params.items():
-            self.writer.add_text(model, str(dict(scores)), global_step = scores['step'])
