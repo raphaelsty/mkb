ebdd82449f645ac16e73d9bec38834646d3bb9e9
raphaelsty <raphael.sourty@gmail.com>
2020-09-18 16:52:01 +0200

    Update ConvE

--- kdmkb/losses/__init__.py
+++ kdmkb/losses/__init__.py
@@ -1,7 +1,9 @@
 from .adversarial import Adversarial
+from .bce import BCEWithLogitsLoss
 from .kl_divergence import KlDivergence
 
 __all__ = [
     'Adversarial',
+    'BCEWithLogitsLoss',
     'KlDivergence',
 ]
---
+++ kdmkb/losses/bce.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+__all__ = ['BCEWithLogitsLoss']
+
+
+class BCEWithLogitsLoss(nn.BCEWithLogitsLoss):
+
+    def __init__(self):
+        super().__init__()
